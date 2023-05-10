82f8375495efec02f966bc12cc67c363b3d24722
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-05-10 19:06:38 -0300

    creacion del proyecto y coneccion a db

---
+++ src/main/java/com/code/BillMyCode/BillMyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.code.BillMyCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillMyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillMyCodeApplication.class, args);
+	}
+
+}
